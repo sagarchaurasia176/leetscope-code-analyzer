1b0628f2f81a1c3178fb1f655766382ec1ca9cf8
sagarchaurasia176 <sagar.volgainfosys@gmail.com>
2026-01-22 17:24:37 +0530

    fix:the ui issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import ArchitectureAndUsed from './components/ArchitectureAndUsed'
 import Footer from './components/Footer'
 import HeroSectionOne from './components/HeroSection'
+import VideoSection from './components/VideoSection'
 
 const App = () => {
   return (
     <div>
       {/* other-components   */}
       <HeroSectionOne/>
+    
+      {/* Video-sections */}
+      <VideoSection/>
       {/* architecture */}
       <ArchitectureAndUsed/>
       <Footer/>
---
+++ src/components/VideoSection.tsx
@@ -0,0 +1,44 @@
+import { div } from 'motion/react-client'
+import React from 'react'
+
+const VideoSection = () => {
+  return (
+    <section className='w-full min-h-screen bg-gradient-to-b from-gray-900 to-black'>
+      <div className='container mx-auto px-4 py-16'>
+        <div className='text-center mb-12'>
+          <h1 className='text-4xl md:text-6xl font-bold text-white mb-4 tracking-tight'>
+           How It Works ? 
+          </h1>
+          <p className='text-xl text-gray-300 max-w-2xl mx-auto'>
+           Your intelligent companion for LeetCode problem-solving. Get instant Big O complexity analysis directly on LeetCode platform.
+          </p>
+        </div>
+
+        <div className="relative w-full max-w-6xl mx-auto rounded-2xl overflow-hidden shadow-2xl">
+          <div className="relative aspect-video bg-black">
+            <video
+              className="absolute inset-0 w-full h-full object-cover"
+              autoPlay
+              muted
+              loop
+              playsInline
+              preload="metadata"
+            >
+              <source
+                src="https://res.cloudinary.com/djfixzkqe/video/upload/v1769082411/Purple_canyon_YouTube_intro_-_Made_with_Clipchamp_qww4vh.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+
+            <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent pointer-events-none"></div>
+    </div>
+        </div>
+        
+        
+      </div>
+    </section>
+  )
+}
+
+export default VideoSection
